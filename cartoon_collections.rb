82cb98acf674446d393465dc7db93dc92ff880f9
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-31 00:05:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def greet_characters(array)
   array.each do |greet|
-  puts "#{Hello, #{greet.capitilize}!"
+  puts "#{Hello, #{greet.capitalize}!"
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
