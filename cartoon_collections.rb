05359fa0f91a58e2b8fb98eb453ebe33794e7a89
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-31 00:14:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  total = 1
   array.each_with_index do |list|
-    puts "#{list.capitalize}"
+    puts list.capitalize}
   end
-  total += 1
 end
